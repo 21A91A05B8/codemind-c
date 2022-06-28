cf9cc17b2d7cd87477ba3ab2ec72dce9f172b89d
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-06-28 23:11:00 +0530

    #include<stdio.h> int main() {     int c=0,n,k=0,temp,i,a[100],j,b[100],count=0;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         count=0;         for(j=1;j<=i;j++)         {             if(i%j==0)             {                 count++;             }         }         if(count==2)         {             b[k]=i;             k++;         }     }     for(i=0;i<k;i++)     {         for(j=i+1;j<k;j++)         {             if(b[i]*b[j]==n)             {                 printf("%d %d",b[i],b[j]);                 c++;                 break;             }         }     }     if(c==0)     {         printf("-1");     } }

--- Find_two_distinct_prime_numbers_with_given_product.c
+++ Find_two_distinct_prime_numbers_with_given_product.c
@@ -1,38 +1,38 @@
 #include<stdio.h>
 int main()
 {
-	int n,count=0,i,j,k,c=0,a[100],b[100];
-	scanf("%d",&n);
-	for(i=0;i<=n;i++)
-	{
-		count=0;
-		for(j=1;j<=i;j++)
-		{
-			if(i%j==0)
-			{
-				count++;
-			}
-		}
-		if(count==2)
-		{
-			b[k]=i;
-			k++;
-		}
-	}
-	for(i=0;i<k;i++)
-	{
-		for(j=i+1;j<k;j++)
-		{
-			if(b[i]*b[j]==n)
-			{
-				printf("%d %d",b[i],b[j]);
-				c++;
-				break;
-			}
-		}
-	}
-	if(c==0)
-	{
-		printf("-1");
-	}
+    int c=0,n,k=0,temp,i,a[100],j,b[100],count=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        count=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            b[k]=i;
+            k++;
+        }
+    }
+    for(i=0;i<k;i++)
+    {
+        for(j=i+1;j<k;j++)
+        {
+            if(b[i]*b[j]==n)
+            {
+                printf("%d %d",b[i],b[j]);
+                c++;
+                break;
+            }
+        }
+    }
+    if(c==0)
+    {
+        printf("-1");
+    }
 }
