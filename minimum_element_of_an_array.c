24126ae82c147a0f525590beb8101c06b99154cf
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-07-02 23:07:38 +0530

    #include<stdio.h> int main() {     int arr[100],n,min,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     min=arr[0];     for(i=0;i<n;i++)     {         if(min>arr[i])         {             min=arr[i];         }     }     printf("%d",min);
    }

---
+++ minimum_element_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,min,i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    min=arr[0];
+    for(i=0;i<n;i++)
+    {
+        if(min>arr[i])
+        {
+            min=arr[i];
+        }
+    }
+    printf("%d",min);
+
+}
