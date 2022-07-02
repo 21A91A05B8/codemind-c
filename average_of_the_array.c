08869b33c9bde9eac33bca4a3acc39618978cbd0
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-07-02 23:14:03 +0530

    #include<stdio.h> int main() {     int arr[100],n,min,i,sum=0;     float avg;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         sum=sum+arr[i];         avg=float(sum)/n;     }     printf("%.2f",avg);
    }

---
+++ average_of_the_array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int arr[100],n,min,i,sum=0;
+    float avg;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+arr[i];
+        avg=float(sum)/n;
+    }
+    printf("%.2f",avg);
+    
+
+}
