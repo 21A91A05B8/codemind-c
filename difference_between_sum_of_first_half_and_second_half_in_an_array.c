22cb44750b99e566b78e0054fa937caaf5c262d1
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-07-03 16:38:47 +0530

    #include<stdio.h> int  main() {     int s1=0,arr[100],n,s2,diff,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n/2;i++)     {         s1=s1+arr[i];     }     for(i=n/2;i<n;i++)     {         s2=s2+arr[i];     }     diff=s2-s1;     printf("%d",diff);
    }

--- difference_between_sum_of_first_half_and_second_half_in_an_array.c
+++ difference_between_sum_of_first_half_and_second_half_in_an_array.c
@@ -1,30 +1,21 @@
 #include<stdio.h>
-int main()
+int  main()
 {
-    int n,i,s1=0,s2=0,arr[100],sub;
+    int s1=0,arr[100],n,s2,diff,i;
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
         scanf("%d",&arr[i]);
     }
-    for(i=0;i<n;i++)
+    for(i=0;i<n/2;i++)
     {
-        for(i=0;i<n/2;i++)
-        {
-            s1=s1+arr[i];
-        }
-        for(i=n/2;i<n;i++)
-        {
-            s2=s2+arr[i];
-        }
+        s1=s1+arr[i];
     }
-    if(s1>s2)
+    for(i=n/2;i<n;i++)
     {
-        sub=s1-s2;
+        s2=s2+arr[i];
     }
-    else
-    {
-        sub=s2-s1;
-    }
-    printf("%d",sub);
+    diff=s2-s1;
+    printf("%d",diff);
+    
 }
