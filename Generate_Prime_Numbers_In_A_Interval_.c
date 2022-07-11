5a31d900f3c8f7ca7ef104d4119b4befd17d5585
21A91A05B8 <97424421+21A91A05B8@users.noreply.github.com>
2022-07-11 18:41:14 +0530

    #include<stdio.h> int main() {      int i,n,m,j,c=0;     scanf("%d%d",&n,&m);     for(i=n;i<=m;i++)     {         c=0;         for(j=1;j<=i;j++)         {             if(i%j==0)             {                 c++;             }         }         if(c==2)         {             printf("%d ",i);         }     } }

--- Generate_Prime_Numbers_In_A_Interval_.c
+++ Generate_Prime_Numbers_In_A_Interval_.c
@@ -1,24 +1,22 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-   int n1,n2,i,j,c=0;
-   scanf("%d %d",&n1,&n2);
-   for(i=n1+1;i<n2;i++)
-   {
-      c=0;
-      for(j=2;j<=i/2;++j)
-      { 
-         if(i%j==0)
-         {
-            c++;
-        
-         }
-      }
-      if(c==0)
-      {
-         printf("%d
+     int i,n,m,j,c=0;
+    scanf("%d%d",&n,&m);
+    for(i=n;i<=m;i++)
+    {
+        c=0;
+        for(j=1;j<=i;j++)
+        {
+            if(i%j==0)
+            {
+                c++;
+            }
+        }
+        if(c==2)
+        {
+            printf("%d
 ",i);
-      }
-   }
-
+        }
+    }
 }
